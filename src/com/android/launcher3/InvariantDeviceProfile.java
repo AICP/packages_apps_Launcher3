9388ad291c001a920698a22f0d45a95d80f2a6c6
Joey <joey@lineageos.org>
2018-10-10 08:27:37 +0200

    Trebuchet: add toggle for desktop and drawer labels
    Squashed with:
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Wed Jan 8 23:53:25 2020 -0600
    
        Trebuchet: Fix missing icon labels in folders
    
        Fixes regression introduced in 186796e.
    
        Change-Id: I1e6054fe84ea54bf2a85bf6d26760196ffe78f69
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   2021-01-13 17:01:22 +0100
    
        Trebuchet: Honor desktop show labels preference for folder icons
    
        Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/2937
    
        Change-Id: I5769057809890cd8086ec01d6e9d1be87fe17c25
    
    Change-Id: I98063b7adaf22029c4bfa50d2cac730f3612e121
    Signed-off-by: Joey <joey@lineageos.org>
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- src/com/android/launcher3/InvariantDeviceProfile.java
+++ src/com/android/launcher3/InvariantDeviceProfile.java
@@ -27,6 +27,8 @@ import static com.android.launcher3.util.Executors.MAIN_EXECUTOR;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.content.res.XmlResourceParser;
@@ -74,7 +76,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class InvariantDeviceProfile {
+public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener {
 
     public static final String TAG = "IDP";
     // We do not need any synchronization for this variable as its only written on UI thread.
@@ -90,6 +92,9 @@ public class InvariantDeviceProfile {
     public static final int TYPE_TABLET = 2;
 
     private static final float ICON_SIZE_DEFINED_IN_APP_DP = 48;
+
+    public static final String KEY_SHOW_DESKTOP_LABELS = "pref_desktop_show_labels";
+    public static final String KEY_SHOW_DRAWER_LABELS = "pref_drawer_show_labels";
 
     // Constants that affects the interpolation curve between statically defined device profile
     // buckets.
@@ -217,6 +222,8 @@ public class InvariantDeviceProfile {
 
     public Point defaultWallpaperSize;
 
+    private Context mContext;
+
     private final ArrayList<OnIDPChangeListener> mChangeListeners = new ArrayList<>();
 
     @VisibleForTesting
@@ -224,6 +231,10 @@ public class InvariantDeviceProfile {
 
     @TargetApi(23)
     private InvariantDeviceProfile(Context context) {
+        mContext = context;
+
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context);
+        prefs.registerOnSharedPreferenceChangeListener(this);
         String gridName = getCurrentGridName(context);
         String newGridName = initGrid(context, gridName);
         if (!newGridName.equals(gridName)) {
@@ -338,6 +349,13 @@ public class InvariantDeviceProfile {
             return TYPE_TABLET;
         } else {
             return TYPE_PHONE;
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+        if (KEY_SHOW_DESKTOP_LABELS.equals(key) || KEY_SHOW_DRAWER_LABELS.equals(key)) {
+            onConfigChanged(mContext);
         }
     }
 
