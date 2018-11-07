ad6280444fcd6985f30b6aec889e84e29a794511
ezio84 <brabus84@gmail.com>
2018-11-07 23:48:51 +0100

    New Recents: add kill action to app shortcuts popup
    Change-Id: Iea088a79d2be5e552e510f0ac153eb4c1976e383

--- quickstep/recents_ui_overrides/src/com/android/quickstep/TaskOverlayFactory.java
+++ quickstep/recents_ui_overrides/src/com/android/quickstep/TaskOverlayFactory.java
@@ -112,7 +112,8 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
             TaskShortcutFactory.PIN,
             TaskShortcutFactory.INSTALL,
             TaskShortcutFactory.FREE_FORM,
-            TaskShortcutFactory.WELLBEING
+            TaskShortcutFactory.WELLBEING,
+            TaskShortcutFactory.KILL_APP
     };
 
     /**
