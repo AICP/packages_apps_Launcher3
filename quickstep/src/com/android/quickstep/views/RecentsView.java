659da53884c8008df32b1aa6efa5d9fbee20f3fb
SpiritCroc <spiritcroc@gmail.com>
2018-11-13 11:24:08 +0300

    Launcher3: quickstep: animate AICP again
    w16.1: add back empty recents
    
    Change-Id: I1bf0425b0f2dc4369b3946bece8350f88f0bf1d3

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -97,6 +97,7 @@ import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.SystemClock;
@@ -744,7 +745,7 @@ public abstract class RecentsView<
     protected boolean mLoadPlanEverApplied;
 
     // Variables for empty state
-    private final Drawable mEmptyIcon;
+    private final AnimatedVectorDrawable mEmptyIcon;
     private final CharSequence mEmptyMessage;
     private final TextPaint mEmptyMessagePaint;
     private final Point mLastMeasureSize = new Point();
@@ -975,7 +976,7 @@ public abstract class RecentsView<
         mClampedScrollOffsetBound = getResources().getDimensionPixelSize(
                 R.dimen.transient_taskbar_clamped_offset_bound);
 
-        mEmptyIcon = context.getDrawable(R.drawable.ic_view_carousel);
+        mEmptyIcon = (AnimatedVectorDrawable) context.getDrawable(R.drawable.ic_empty_recents);
         mEmptyIcon.setCallback(this);
         mEmptyMessage = context.getText(R.string.recents_empty_message);
         mEmptyMessagePaint = new TextPaint();
@@ -5606,6 +5607,9 @@ public abstract class RecentsView<
             int left = (mLastMeasureSize.x - mEmptyIcon.getIntrinsicWidth()) / 2;
             mEmptyIcon.setBounds(left, top, left + mEmptyIcon.getIntrinsicWidth(),
                     top + mEmptyIcon.getIntrinsicHeight());
+            if (mEmptyIcon instanceof AnimatedVectorDrawable) {
+                ((AnimatedVectorDrawable) mEmptyIcon).start();
+            }
         }
     }
 
