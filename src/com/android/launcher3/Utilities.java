b0eb9fc4a94e63e23268cb9d6937253bed7a5072
hundeva <hundeva@gmail.com>
2019-10-12 07:33:14 +0000

    Launcher3: Add restart action for toggling settings
    Useful to update some settings that only change on restarting the launcher.
    
    It's a clean up version of the following commits:
    https://github.com/DirtyUnicorns/android_packages_apps_Launcher3/commit/10529fedf53e6f79abe380832dd5e7deca329b14
    https://github.com/DirtyUnicorns/android_packages_apps_Launcher3/commit/a3357bb5f9defef8d91f4f2b663d45e422038447
    https://github.com/DirtyUnicorns/android_packages_apps_Launcher3/commit/74cba5a81ae39c6694bc4b7ddc8cd64440c57bfa
    https://github.com/DirtyUnicorns/android_packages_apps_Launcher3/commit/81e08c2033400625c0701a6bcb5cd9fd4504f5fe
    
    Co-authored-by: Alex Cruz <alex@dirtyunicorns.com>
    Co-authored-by: Ezio Lacandia Bijelkic <brabus84@gmail.com>
    Change-Id: Iac6e61cc00a7afb57ff083f4b065c86d2a7f2c13
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -89,6 +89,7 @@ import com.android.launcher3.pm.ShortcutConfigActivityInfo;
 import com.android.launcher3.shortcuts.ShortcutKey;
 import com.android.launcher3.shortcuts.ShortcutRequest;
 import com.android.launcher3.util.IntArray;
+import com.android.launcher3.util.LooperExecutor;
 import com.android.launcher3.util.PackageManagerHelper;
 import com.android.launcher3.util.SplitConfigurationOptions.SplitPositionOption;
 import com.android.launcher3.util.Themes;
@@ -104,6 +105,8 @@ import java.util.Locale;
 import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static com.android.launcher3.util.Executors.MODEL_EXECUTOR;
 
 /**
  * Various utilities shared amongst the Launcher's classes.
@@ -137,6 +140,8 @@ public final class Utilities {
 
     @ChecksSdkIntAtLeast(api = VERSION_CODES.TIRAMISU, codename = "T")
     public static final boolean ATLEAST_T = Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU;
+
+    private static final long WAIT_BEFORE_RESTART = 250;
 
     /**
      * Set on a motion event dispatched from the nav bar. See {@link MotionEvent#setEdgeFlags(int)}.
@@ -935,5 +940,15 @@ public final class Utilities {
     public static boolean isWorkspaceEditAllowed(Context context) {
         SharedPreferences prefs = getPrefs(context.getApplicationContext());
         return !prefs.getBoolean(InvariantDeviceProfile.KEY_WORKSPACE_LOCK, false);
+    }
+
+    public static void restart(final Context context) {
+        new LooperExecutor(MODEL_EXECUTOR.getLooper()).execute(() -> {
+            try {
+                Thread.sleep(WAIT_BEFORE_RESTART);
+            } catch (Exception ignored) {
+            }
+            android.os.Process.killProcess(android.os.Process.myPid());
+        });
     }
 }
