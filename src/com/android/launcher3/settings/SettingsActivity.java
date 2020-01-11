b58a294d793930527ca3df6c694fbf08fa754804
Amir Zaidi <azaidi@live.nl>
2020-01-11 13:55:58 +0100

    Launcher3: Add Icon Pack Support from Shade Launcher
    @DennySPb: port to A11
    
    Squashed with:
    AospLauncher: Bypass adaptive icon wrapping with icon pack icons
    AospLauncher: Clear all icon overrides when changing global icon pack
    
    Change-Id: Id81da397d5ef33577d6f53f349080c49a61591ab
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -25,6 +25,7 @@ import static com.android.launcher3.util.SecureSettingsObserver.newNotificationS
 
 import static com.aicp.launcher.OverlayCallbackImpl.KEY_ENABLE_MINUS_ONE;
 
+import com.android.launcher3.customization.IconDatabase;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -34,6 +35,10 @@ import android.content.pm.PackageManager;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.MenuItem;
+
+import com.android.launcher3.settings.preference.IconPackPrefSetter;
+import com.android.launcher3.settings.preference.ReloadingListPreference;
+import com.android.launcher3.util.AppReloader;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
@@ -79,9 +84,14 @@ public class SettingsActivity extends FragmentActivity
     public static boolean restartNeeded = false;
     public static final String KEY_TRUST_APPS = "pref_trust_apps";
 
+    private static final String KEY_ICON_PACK = "pref_icon_pack";
+
+    private static Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = getApplicationContext();
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -112,6 +122,10 @@ public class SettingsActivity extends FragmentActivity
                 LauncherAppState.getInstanceNoCreate().setNeedsRestart();
         }
 
+    }
+
+    public interface OnResumePreferenceCallback {
+        void onResume();
     }
 
     @Override
@@ -260,8 +274,16 @@ public class SettingsActivity extends FragmentActivity
                         });
                         return true;
                     });
+                case KEY_ICON_PACK:
+                    ReloadingListPreference icons = (ReloadingListPreference) findPreference(KEY_ICON_PACK);
+                    icons.setOnReloadListener(new IconPackPrefSetter(mContext));
+                    icons.setOnPreferenceChangeListener((pref, val) -> {
+                        IconDatabase.clearAll(mContext);
+                        IconDatabase.setGlobal(mContext, (String) val);
+                        AppReloader.get(mContext).reload();
+                        return true;
+                    });
             }
-
             return true;
         }
 
