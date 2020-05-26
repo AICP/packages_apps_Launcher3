64b8849fd8f26d448e9f58cae6b8d16a6fd56bd0
doc HD <doc.divxm@gmail.com>
2020-05-26 13:56:14 +0300

    Launcher3: Use AICP icon in random messages
    Change-Id: I07def56e6c00dd1704917c145d138fbe62584b4e

--- src/com/android/launcher3/quickspace/QuickEventsController.java
+++ src/com/android/launcher3/quickspace/QuickEventsController.java
@@ -112,7 +112,7 @@ public class QuickEventsController {
         mIsQuickEvent = true;
         mEventTitle = mContext.getResources().getString(R.string.quick_event_rom_intro_welcome);
         mEventTitleSub = mContext.getResources().getStringArray(R.array.welcome_message_variants)[getLuckyNumber(0,6)];
-        mEventSubIcon = R.drawable.ic_quickspace_crdroid;
+        mEventSubIcon = R.drawable.ic_quickspace_aicp;
 
         mEventTitleSubAction = new OnClickListener() {
             @Override
@@ -228,7 +228,7 @@ public class QuickEventsController {
                 if (getLuckyNumber(13) == 7) {
                     psaLength = mPSARandomStr.length - 1;
                     mEventTitleSub = mPSARandomStr[getLuckyNumber(0, psaLength)];
-                    mEventSubIcon = R.drawable.ic_quickspace_crdroid;
+                    mEventSubIcon = R.drawable.ic_quickspace_aicp;
                     mIsQuickEvent = true;
                 } else {
                     mIsQuickEvent = false;
