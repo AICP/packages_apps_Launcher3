722ae7326f9c8e48b835ae3ec1d381e312f46408
Pranav Vashi <neobuddy89@gmail.com>
2021-01-10 16:27:49 +0530

    Launcher3: Use MODEL_EXECUTOR directly
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ibb104052e8abf0e542503415ea06125c02e9b19b

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -87,7 +87,6 @@ import com.android.launcher3.pm.ShortcutConfigActivityInfo;
 import com.android.launcher3.shortcuts.ShortcutKey;
 import com.android.launcher3.shortcuts.ShortcutRequest;
 import com.android.launcher3.util.IntArray;
-import com.android.launcher3.util.LooperExecutor;
 import com.android.launcher3.util.PackageManagerHelper;
 import com.android.launcher3.views.ActivityContext;
 import com.android.launcher3.views.BaseDragLayer;
@@ -895,7 +894,7 @@ public final class Utilities {
     }
 
     public static void restart(final Context context) {
-        new LooperExecutor(MODEL_EXECUTOR.getLooper()).execute(() -> {
+        MODEL_EXECUTOR.execute(() -> {
             try {
                 Thread.sleep(WAIT_BEFORE_RESTART);
             } catch (Exception ignored) {
