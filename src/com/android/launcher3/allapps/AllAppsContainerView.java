0e1ac55eb5fa185cd000bf4e5e49951474a59a42
DennySPb <dennyspb@gmail.com>
2021-12-07 13:47:04 +0300

    Revert "allapps: remove dynamic header background"
    This reverts commit 0d4a4f436bab2965581ccf8d5b0f8fbee727342f.

--- src/com/android/launcher3/allapps/AllAppsContainerView.java
+++ src/com/android/launcher3/allapps/AllAppsContainerView.java
@@ -131,7 +131,7 @@ public class AllAppsContainerView extends SpringRelativeLayout implements DragSo
     private final int mHeaderProtectionColor;
     private final float mHeaderThreshold;
     private ScrimView mScrimView;
-    private final int mHeaderColor = 0xFFFFFF;
+    private int mHeaderColor;
 
     public AllAppsContainerView(Context context) {
         this(context, null);
@@ -794,8 +794,18 @@ public class AllAppsContainerView extends SpringRelativeLayout implements DragSo
 
 
     protected void updateHeaderScroll(int scrolledOffset) {
-        if (scrolledOffset == 0 && mSearchUiManager.getEditText() != null) {
-            mSearchUiManager.getEditText().show();
+        float prog = Math.max(0, Math.min(1, (float) scrolledOffset / mHeaderThreshold));
+        int viewBG = ColorUtils.blendARGB(mScrimColor, mHeaderProtectionColor, prog);
+        int headerColor = ColorUtils.setAlphaComponent(viewBG,
+                (int) (getSearchView().getAlpha() * 255));
+        if (headerColor != mHeaderColor) {
+            mHeaderColor = headerColor;
+            getSearchView().setBackgroundColor(viewBG);
+            getFloatingHeaderView().setHeaderColor(viewBG);
+            invalidateHeader();
+            if (scrolledOffset == 0 && mSearchUiManager.getEditText() != null) {
+                mSearchUiManager.getEditText().show();
+            }
         }
     }
 
