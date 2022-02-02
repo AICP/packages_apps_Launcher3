3ca45d994c4772982cedc3b5a0f971b66125c8c9
Pranav Vashi <neobuddy89@gmail.com>
2022-02-03 03:24:11 +0530

    Launcher3: Add toggle disable hotseat searchbar
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I5e526d88a61f7d186e93f93f4eacedf45be3ed4d

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -154,6 +154,9 @@ public final class Utilities {
     public static final boolean IS_DEBUG_DEVICE =
             Build.TYPE.toLowerCase(Locale.ROOT).equals("eng");
 
+    public static final String GSA_PACKAGE = "com.google.android.googlequicksearchbox";
+    public static final String KEY_DOCK_SEARCH = "pref_dock_search";
+
     /**
      * Returns true if theme is dark.
      */
@@ -949,5 +952,22 @@ public final class Utilities {
             }
             android.os.Process.killProcess(android.os.Process.myPid());
         });
+    }
+
+    public static boolean isGSAEnabled(Context context) {
+        try {
+            return context.getPackageManager().getApplicationInfo(GSA_PACKAGE, 0).enabled;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    public static boolean showQSB(Context context) {
+        return isGSAEnabled(context) && isQSBEnabled(context);
+    }
+
+    private static boolean isQSBEnabled(Context context) {
+        SharedPreferences prefs = getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_DOCK_SEARCH, true);
     }
 }
