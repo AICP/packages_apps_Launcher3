c68c8c83e33950d3e1ba8b1b2ee3321303ad1393
minaripenguin <alexfinhart@gmail.com>
2022-07-16 08:35:11 +0800

    Launcher3: Add an option to customize blur radius
    * Set max blur radius to 175px
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: If73c4655367c6e6ba3ac09aa0574eeb5566be47c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/quickstep/util/BaseDepthController.java
+++ quickstep/src/com/android/quickstep/util/BaseDepthController.java
@@ -39,6 +39,7 @@ import androidx.annotation.VisibleForTesting;
 import com.android.app.animation.Interpolators;
 import com.android.launcher3.Flags;
 import com.android.launcher3.Launcher;
+import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.LauncherState;
 import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
@@ -132,11 +133,8 @@ public class BaseDepthController {
         if (Flags.allAppsBlur() || enableOverviewBackgroundWallpaperBlur()) {
             mCrossWindowBlursEnabled =
                     CrossWindowBlurListeners.getInstance().isCrossWindowBlurEnabled();
-            mMaxBlurRadius = activity.getResources().getDimensionPixelSize(
-                    R.dimen.max_depth_blur_radius_enhanced);
-        } else {
-            mMaxBlurRadius = activity.getResources().getInteger(R.integer.max_depth_blur_radius);
         }
+        mMaxBlurRadius = LauncherPrefs.BLUR_DEPTH.get(mLauncher);
         mWallpaperManager = activity.getSystemService(WallpaperManager.class);
 
         MultiPropertyFactory<BaseDepthController> depthProperty =
