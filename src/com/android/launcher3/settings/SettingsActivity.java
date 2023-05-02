2c8cc8ec68b8b786d2dae3089dfb75e1378acd4c
Semavi Ulusoy <doc.divxm@gmail.com>
2023-05-03 00:12:47 +0300

    Revert "Trebuchet: implement hidden & protected apps"
    This reverts commit 93d10d1b08a978b6fcdf67ca478a8a2b76ab1506.
    
     ** we have ROM sided app-lock and systemwide hide app feature
     ** This feature is broken after r35 merge
    
    Change-Id: I04089b002ed82363d717f28ead33432ad46acdee

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -55,8 +55,6 @@ import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.config.FeatureFlags;
-import com.android.launcher3.lineage.LineageUtils;
-import com.android.launcher3.lineage.trust.TrustAppsActivity;
 import com.android.launcher3.model.WidgetsModel;
 import com.android.launcher3.states.RotationHelper;
 import com.android.launcher3.uioverrides.flags.DeveloperOptionsFragment;
@@ -92,8 +90,6 @@ public class SettingsActivity extends FragmentActivity
     static final String EXTRA_FRAGMENT = ":settings:fragment";
     @VisibleForTesting
     static final String EXTRA_FRAGMENT_ARGS = ":settings:fragment_args";
-
-    public static final String KEY_TRUST_APPS = "pref_trust_apps";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -321,16 +317,6 @@ public class SettingsActivity extends FragmentActivity
                 case KEY_ENABLE_MINUS_ONE:
                     mShowGoogleAppPref = preference;
                     updateIsGoogleAppEnabled();
-
-                case KEY_TRUST_APPS:
-                    preference.setOnPreferenceClickListener(p -> {
-                         LineageUtils.showLockScreen(getActivity(),
-                                getString(R.string.trust_apps_manager_name), () -> {
-                            Intent intent = new Intent(getActivity(), TrustAppsActivity.class);
-                            startActivity(intent);
-                        });
-                        return true;
-                    });
                     return true;
             }
 
--- src/com/android/launcher3/touch/ItemClickHandler.java
+++ src/com/android/launcher3/touch/ItemClickHandler.java
@@ -28,7 +28,6 @@ import static com.android.launcher3.util.Executors.UI_HELPER_EXECUTOR;
 
 import android.app.AlertDialog;
 import android.app.PendingIntent;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.LauncherApps;
@@ -46,7 +45,6 @@ import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.folder.Folder;
 import com.android.launcher3.folder.FolderIcon;
-import com.android.launcher3.lineage.trust.db.TrustDatabaseHelper;
 import com.android.launcher3.logging.InstanceId;
 import com.android.launcher3.logging.InstanceIdSequence;
 import com.android.launcher3.logging.StatsLogManager;
@@ -351,16 +349,7 @@ public class ItemClickHandler {
             // Preload the icon to reduce latency b/w swapping the floating view with the original.
             FloatingIconView.fetchIcon(launcher, v, item, true /* isOpening */);
         }
-
-        TrustDatabaseHelper db = TrustDatabaseHelper.getInstance(launcher);
-        ComponentName cn = item.getTargetComponent();
-        boolean isProtected = cn != null && db.isPackageProtected(cn.getPackageName());
-
-        if (isProtected) {
-            launcher.startActivitySafelyAuth(v, intent, item);
-        } else {
-            launcher.startActivitySafely(v, intent, item);
-        }
+        launcher.startActivitySafely(v, intent, item);
     }
 
     /**
