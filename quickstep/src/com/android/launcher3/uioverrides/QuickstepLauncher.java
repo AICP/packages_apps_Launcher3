1914de1d2f5f7ccc89eef17e9a3f5c80549476ab
Pranav Vashi <neobuddy89@gmail.com>
2023-06-25 21:35:05 +0530

    Launcher3: Add UNINSTALL shortcut to QuickstepLauncher
    Change-Id: I25edf15782f884cbdf6ddc239e1b0cdfedded998
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -60,6 +60,7 @@ import static com.android.launcher3.popup.SystemShortcut.DONT_SUGGEST_APP;
 import static com.android.launcher3.popup.SystemShortcut.INSTALL;
 import static com.android.launcher3.popup.SystemShortcut.PRIVATE_PROFILE_INSTALL;
 import static com.android.launcher3.popup.SystemShortcut.REMOVE;
+import static com.android.launcher3.popup.SystemShortcut.UNINSTALL;
 import static com.android.launcher3.popup.SystemShortcut.UNINSTALL_APP;
 import static com.android.launcher3.popup.SystemShortcut.WIDGETS;
 import static com.android.launcher3.taskbar.LauncherTaskbarUIController.ALL_APPS_PAGE_PROGRESS_INDEX;
@@ -553,6 +554,7 @@ public class QuickstepLauncher extends Launcher implements RecentsViewContainer,
         }
 
         shortcuts.addAll(getSplitShortcuts());
+        shortcuts.add(UNINSTALL);
         shortcuts.add(WIDGETS);
         shortcuts.add(INSTALL);
         // TODO(b/444744861): Update private space apps to have its own container.
