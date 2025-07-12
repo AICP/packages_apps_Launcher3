25ae5febe127636a36f2f922769c05c0d278e633
SpiritCroc <dev@spiritcroc.de>
2025-07-12 16:24:09 +0200

    Build slim recents into launcher, w16.1 edition
    Change-Id: Id2b714e86e633a8f7d43d2e7c2126229c1f7fac4

--- quickstep/src/com/android/quickstep/OverviewCommandHelper.kt
+++ quickstep/src/com/android/quickstep/OverviewCommandHelper.kt
@@ -90,6 +90,7 @@ constructor(
         CoroutineScope(SupervisorJob() + dispatcherProvider.lightweightBackground)
 
     private val commandQueue = ConcurrentLinkedDeque<CommandInfo>()
+    private var aicpCommandHelper: AicpOverviewCommandHelper? = null
 
     /**
      * Index of the TaskView that should be focused when launching Overview. Persisted so that we do
@@ -216,6 +217,16 @@ constructor(
     fun executeCommand(command: CommandInfo, onCallbackResult: () -> Unit): Boolean {
         val recentsView = getVisibleRecentsView(command.displayId)
         OverviewCommandHelperProtoLogProxy.logExecutingCommand(command, recentsView)
+        val aicpHelper = aicpCommandHelper ?: run {
+            getContainerInterface(command.displayId)?.getCreatedContainer()?.asContext()?.let {
+                AicpOverviewCommandHelper(it.applicationContext)
+            }?.also {
+                aicpCommandHelper = it
+            }
+        }
+        if (aicpHelper?.overrideExecuteCommand(command.type) == true) {
+            return true
+        }
         return if (recentsView != null) {
             executeWhenRecentsIsVisible(command, recentsView, onCallbackResult)
         } else {
