a07dec85f314cebb1d221efc162cd5041f13a4bc
SpiritCroc <dev@spiritcroc.de>
2025-07-12 16:24:09 +0200

    Build slim recents into launcher
    Change-Id: Id2b714e86e633a8f7d43d2e7c2126229c1f7fac4

---
+++ quickstep/src/com/android/quickstep/AicpOverviewCommandHelper.kt
@@ -0,0 +1,67 @@
+package com.android.quickstep
+
+import android.content.ContentResolver;
+import android.content.Context
+import android.database.ContentObserver
+import android.os.Handler
+import android.provider.Settings
+import com.android.systemui.slimrecent.RecentController
+
+class AicpOverviewCommandHelper(
+    val context: Context
+) {
+    private var useSlimRecents = false
+
+    private val slimRecents = RecentController().also {
+        it.onStart(context)
+    }
+
+    private val settingsObserver = object : ContentObserver(Handler()) {
+        fun observe() {
+            val resolver: ContentResolver = context.getContentResolver()
+            resolver.registerContentObserver(
+                Settings.System.getUriFor(
+                    Settings.System.USE_SLIM_RECENTS
+                ), false, this
+            )
+            update()
+        }
+
+        fun unobserve() { // TODO call onDestroy of launcher (could use lifecycleTracker?)
+            context.getContentResolver().unregisterContentObserver(this)
+        }
+
+        public override fun onChange(selfChange: Boolean) {
+            update()
+        }
+
+        fun update() {
+            val resolver = context.getContentResolver()
+
+            useSlimRecents = Settings.System.getInt(resolver, Settings.System.USE_SLIM_RECENTS, 0) == 1
+        }
+    }.also {
+        it.observe()
+    }
+
+
+    fun overrideExecuteCommand(type: OverviewCommandHelper.CommandType): Boolean {
+        if (!useSlimRecents) {
+            return false
+        }
+        return when (type) {
+            OverviewCommandHelper.CommandType.TOGGLE -> {
+                slimRecents.toggleRecentApps()
+                true
+            }
+            OverviewCommandHelper.CommandType.SHOW -> {
+                slimRecents.showRecentApps()
+                true
+            }
+            OverviewCommandHelper.CommandType.HIDE -> {
+                slimRecents.hideRecents(false)
+            }
+            else -> false
+        }
+    }
+}
--- quickstep/src/com/android/quickstep/OverviewCommandHelper.kt
+++ quickstep/src/com/android/quickstep/OverviewCommandHelper.kt
@@ -73,6 +73,7 @@ constructor(
     private val coroutineScope = CoroutineScope(SupervisorJob() + dispatcherProvider.background)
 
     private val commandQueue = ConcurrentLinkedDeque<CommandInfo>()
+    private var aicpCommandHelper: AicpOverviewCommandHelper? = null
 
     /**
      * Index of the TaskView that should be focused when launching Overview. Persisted so that we do
@@ -169,6 +170,16 @@ constructor(
     fun executeCommand(command: CommandInfo, onCallbackResult: () -> Unit): Boolean {
         val recentsView = visibleRecentsView
         Log.d(TAG, "executeCommand: $command - visibleRecentsView: $recentsView")
+        val aicpHelper = aicpCommandHelper ?: run {
+            containerInterface.getCreatedContainer()?.asContext()?.let {
+                AicpOverviewCommandHelper(it.applicationContext)
+            }?.also {
+                aicpCommandHelper = it
+            }
+        }
+        if (aicpHelper?.overrideExecuteCommand(command.type) == true) {
+            return true
+        }
         return if (recentsView != null) {
             executeWhenRecentsIsVisible(command, recentsView, onCallbackResult)
         } else {
