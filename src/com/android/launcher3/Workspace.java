a2be738636506ed17c6d7b13d70c0b0d051ac66c
mosimchah <mosimchah@gmail.com>
2026-04-20 19:56:06 -0400

    Launcher3: Show icons in desktop mode
    Change-Id: Ie45c1a56763fbe9a6b06f6d0bbe368463e89e776

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -26,6 +26,7 @@ import static com.android.launcher3.LauncherSettings.Favorites.CONTAINER_HOTSEAT
 import static com.android.launcher3.LauncherSettings.Favorites.CONTAINER_HOTSEAT_PREDICTION;
 import static com.android.launcher3.LauncherSettings.Favorites.ITEM_TYPE_APPWIDGET;
 import static com.android.launcher3.LauncherSettings.Favorites.ITEM_TYPE_FILE_SYSTEM_FILE;
+import com.android.launcher3.LauncherState;
 import static com.android.launcher3.LauncherState.ALL_APPS;
 import static com.android.launcher3.LauncherState.EDIT_MODE;
 import static com.android.launcher3.LauncherState.FLAG_MULTI_PAGE;
@@ -113,6 +114,7 @@ import com.android.launcher3.graphics.DragPreviewProvider;
 import com.android.launcher3.homescreenfiles.HomeScreenFilesProvider;
 import com.android.launcher3.icons.BitmapRenderer;
 import com.android.launcher3.icons.FastBitmapDrawable;
+import com.android.launcher3.LauncherState;
 import com.android.launcher3.logger.LauncherAtom;
 import com.android.launcher3.logging.InstanceId;
 import com.android.launcher3.logging.StatsLogManager;
@@ -1509,7 +1511,7 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        mHasOnLayoutBeenCalled = true; // b/349929393 - is the required call to onLayout not done?
+        mHasOnLayoutBeenCalled = true; 
         if (mUnlockWallpaperFromDefaultPageOnLayout) {
             mWallpaperOffset.setLockToDefaultPage(false);
             mUnlockWallpaperFromDefaultPageOnLayout = false;
@@ -1520,6 +1522,18 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
         }
         super.onLayout(changed, left, top, right, bottom);
         updatePageAlphaValues();
+        if (mLauncher.getDeviceProfile().inv.deviceType == InvariantDeviceProfile.TYPE_DESKTOP) {
+            this.setAlpha(1f);
+            this.setVisibility(VISIBLE);
+            int childCount = getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                android.view.View child = getChildAt(i);
+                if (child != null) {
+                    child.setAlpha(1f);
+                    child.setVisibility(VISIBLE);
+                }
+            }
+        }
     }
 
     @Override
