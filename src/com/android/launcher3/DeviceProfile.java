c8010cf9f9e9e6b8b5eb639291f05d9358906ae7
mosimchah <mosimchah@gmail.com>
2026-04-20 19:56:06 -0400

    Launcher3: Show icons in desktop mode
    Change-Id: Ie45c1a56763fbe9a6b06f6d0bbe368463e89e776

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -558,6 +558,10 @@ public class DeviceProfile {
                 getWorkspaceIconProfile().getIconSizePx(), dotRendererCache);
         mDotRendererAllApps = createDotRenderer(
                 getAllAppsProfile().getIconSizePx(), dotRendererCache);
+                if (inv.deviceType == InvariantDeviceProfile.TYPE_DESKTOP) {
+            // This ensures the workspace is not hidden when in desktop mode
+            this.isTaskbarPresent = true; 
+        }
     }
 
     private boolean isLandscapeOrientation()  {
@@ -817,9 +821,11 @@ public class DeviceProfile {
      * It is important to call this method after the All Apps variables have been set.
      */
     private void hideWorkspaceLabelsIfNotEnoughSpace() {
-        // We want enough space so that the text is closer to its corresponding icon.
+        if (inv.deviceType == InvariantDeviceProfile.TYPE_DESKTOP) {
+            return; 
+        }
+
         if (getWorkspaceIconProfile().isLabelHidden()) {
-            // TODO(420933882) Group all modifications of AllAppsProfile in one place
             mAllAppsProfile = AllAppsProfile.Factory.autoResizeAllAppsCells(getAllAppsProfile());
         }
     }
