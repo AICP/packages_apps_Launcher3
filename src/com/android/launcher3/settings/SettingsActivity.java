bcac3e888856b081c5a5863d698f135a02be32f3
mosimchah <mosimchah@gmail.com>
2026-04-20 14:13:20 -0400

    Launcher3: Add option for custom grid
    Change-Id: I8cefd5eba65f718e8d8eeef685fbe5f48be8bf77

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -271,6 +271,12 @@ public class SettingsActivity extends FragmentActivity
             if (getActivity() != null && !TextUtils.isEmpty(getPreferenceScreen().getTitle())) {
                 getActivity().setTitle(getPreferenceScreen().getTitle());
             }
+            // Link the UI elements to the listener
+            Preference rowPref = findPreference("custom_grid_rows");
+            Preference colPref = findPreference("custom_grid_cols");
+
+            if (rowPref != null) rowPref.setOnPreferenceChangeListener(this);
+            if (colPref != null) colPref.setOnPreferenceChangeListener(this);
         }
 
         private boolean isKeyInPreferenceGroup(String targetKey, PreferenceGroup parent) {
@@ -476,6 +482,19 @@ public class SettingsActivity extends FragmentActivity
             return position >= 0 ? new PreferenceHighlighter(
                     list, position, screen.findPreference(mHighLightKey))
                     : null;
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String key = preference.getKey();
+            if ("custom_grid_rows".equals(key) || "custom_grid_cols".equals(key)) {    
+        InvariantDeviceProfile.INSTANCE.get(getContext()).reinit(getContext());
+            if (getActivity() != null) {
+            getActivity().recreate();
+        }
+                return true;
+        }
+        return super.onPreferenceChange(preference, newValue);
         }
     }
 }
