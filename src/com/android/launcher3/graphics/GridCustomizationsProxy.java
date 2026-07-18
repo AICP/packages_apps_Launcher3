41315827fa3bb16f1fbb0ecf8edcfffdb1ad3aad
mosimchah <mosimchah@gmail.com>
2026-07-18 16:12:35 -0400

    Launcher3: expose custom grid for themepicker
    Change-Id: Icfaff77f1ae6664f587bbcecf2eaf260f28438fd

--- src/com/android/launcher3/graphics/GridCustomizationsProxy.java
+++ src/com/android/launcher3/graphics/GridCustomizationsProxy.java
@@ -352,6 +352,22 @@ public class GridCustomizationsProxy implements ProxyProvider {
         return switch (method) {
             case METHOD_GET_PREVIEW -> getPreview(extras);
             case METHOD_GET_PREVIEW_BITMAP -> getPreviewBitmap(extras);
+            case "set_grid" -> {
+                Context ctx = this.mContext; 
+                com.android.launcher3.Utilities.getPrefs(ctx).edit()
+                        .putString("idp_grid_name", arg)
+                        .apply();
+                com.android.launcher3.InvariantDeviceProfile.INSTANCE.get(ctx).reinitAndClear(ctx);
+                android.net.Uri gridUri = new android.net.Uri.Builder()
+                        .scheme("content")
+                        .authority(ctx.getPackageName() + ".grid_control")
+                        .appendPath("grid")
+                        .build();
+                ctx.getContentResolver().notifyChange(gridUri, null);
+                Bundle result = new Bundle();
+                result.putBoolean("serialize_sync", true);
+                yield result;
+            }
             default -> null;
         };
     }
